49a48b9b308507e7d2d288e4981b8e61711b0d46
Sowmen Rahman <sowmen.appd@gmail.com>
2019-04-13 17:26:12 +0600

    added Gamemanager.cs and Audiomanager.cs

---
+++ GameManager.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+  #region Singleton
+
+  private static GameManager _instance;
+  public static GameManager Instance
+  {
+    get
+    {
+      return _instance;
+    }
+    private set
+    {
+      _instance = value;
+    }
+  }
+
+  #endregion
+
+  public bool GameOver
+  {
+    get; set;
+  }
+
+  public bool Paused
+  {
+    get; set;
+  }
+
+  public System.Action<int> onPlayerDeath;
+  public System.Action<int> onPlayerRoundLose;
+  public System.Action onRoundRestart;
+  public System.Action<int> onGameOver;
+  public System.Action<bool> onPause;
+
+  void Awake()
+  {
+    if (Instance != null)
+    {
+      Destroy(gameObject);
+    }
+    else
+    {
+      Instance = this;
+    }
+    DontDestroyOnLoad(gameObject);
+    onPause += Pause;
+  }
+
+  void Start()
+  {
+    Instance.GameOver = false;
+    onPause(false);
+
+    SceneManager.sceneLoaded += (e, ex) =>
+    {
+    };
+  }
+
+  public void LoadScene(string sceneName)
+  {
+    SceneManager.LoadScene(sceneName);
+  }
+
+  public void Pause(bool b)
+  {
+    Paused = b;
+    print(Paused ? "Paused" : "Unpaused");
+  }
+
+
+}
